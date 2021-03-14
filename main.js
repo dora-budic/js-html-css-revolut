144a143a5deac0a12d70245f87c4ab9f2aab2116
Dora Budić <dbudic0706@gmail.com>
2021-03-14 12:52:33 +0100

    Finished

--- main.js
+++ main.js
@@ -4,27 +4,29 @@ $(document).ready(function () {
 
 function dropdownMenu() {
   // Prendo i link della navbar
-  var links = $('.header-right .menu li');
+  var links = $('.header-center .menu li');
 
   links.mouseenter(function() {
     // Prendo la posizione di this
     var linkPos = links.index(this);
 
     // Prendo i dropdown menus
-    var menus = $('.header-right .dropdown-menus > div');
+    var menus = $('.header-center .dropdown-menus > div');
 
     // Prendo il menu con la stessa posizione di this e gli do la classe active
-    menus.eq(linkPos).addClass('active');
+    var menuPos = menus.eq(linkPos);
+    menuPos.addClass('active');
+
+    // Quandro entro nell' altro li, tolgo la classe al menu aperto
+    $(this).siblings('li').mouseenter (function() {
+      menuPos.removeClass('active');
+    });
+
+    // Quando esco dal menu o dallo div che contiene i link tolgo la classe al menu
+    $('.dropdown-menus, .header-center').mouseleave(function () {
+      menuPos.removeClass('active');
+    });
+
   });
 
-  links.mouseleave(function() {
-    // Prendo la posizione di this
-    var linkPos = links.index(this);
-
-    // Prendo i dropdown menus
-    var menus = $('.header-right .dropdown-menus > div');
-
-    // Prendo il menu con la stessa posizione di this e gli tolgo la classe active
-    menus.eq(linkPos).removeClass('active');
-  });
 }
